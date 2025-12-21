a9f8546996f40e994b45a7c75e78f95f761f0c9b
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-21 19:55:04 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,16 +2,26 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // lưu hash ở đây
   role: { type: String, enum: ["user", "admin"], default: "user" }
 });
 
-userSchema.pre("save", async function(next) {
+// Tự động hash mật khẩu trước khi lưu
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
-module.exports = mongoose.model("User", userSchema);
+// Thêm method để so sánh mật khẩu khi login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
+module.exports = mongoose.model("User", userSchema);
