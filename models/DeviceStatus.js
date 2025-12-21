7fa352fcc916db5ba3ec4e3fc25d23e4f4504a86
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-21 22:42:04 +0700

    Update DeviceStatus.js

--- models/DeviceStatus.js
+++ models/DeviceStatus.js
@@ -1,1 +1,15 @@
+const mongoose = require("mongoose");
+
+const DeviceStatusSchema = new mongoose.Schema({
+  deviceId: { type: String },
+  led1: { type: Boolean },
+  led2: { type: Boolean },
+  led3: { type: Boolean },
+  led4: { type: Boolean },
+  fan: { type: Boolean },
+  curtainMode: { type: Number }, // 0=STOP,1=CLOSE,2=OPEN
+  timestamp: { type: Date, default: Date.now }
+}, { versionKey: false });
+
+module.exports = mongoose.model("DeviceStatus", DeviceStatusSchema);
 
