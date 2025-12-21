29657d72f6d33501f6827ff2265eb29f3f90a590
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-21 18:56:13 +0700

    Update Sensor.js

--- models/Sensor.js
+++ models/Sensor.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose");
+const sensorSchema = new mongoose.Schema({
+  sensorType: String,
+  value: Number,
+  timestamp: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model("Sensor", sensorSchema);
 
