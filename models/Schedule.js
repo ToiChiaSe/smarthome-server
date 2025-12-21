2bb64aed89fa163ada8d85c55b2243862d743772
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-21 22:40:17 +0700

    Update Schedule.js

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-const scheduleSchema = new mongoose.Schema({
-  deviceId: String,
-  date: String,
-  time: String,
-  action: String
-});
-module.exports = mongoose.model("Schedule", scheduleSchema);
 
+const ScheduleSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  time: { type: String, required: true }, // "HH:MM"
+  topic: { type: String, required: true },
+  cmd: { type: String, required: true },
+  enabled: { type: Boolean, default: true }
+}, { versionKey: false });
+
+module.exports = mongoose.model("Schedule", ScheduleSchema);
