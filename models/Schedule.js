5c862a43e0b0264cb8b96876e0eae18889099d97
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-22 00:05:21 +0700

    Update Schedule.js

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const ScheduleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  time: { type: String, required: true }, // "HH:MM"
-  topic: { type: String, required: true },
-  cmd: { type: String, required: true },
-  enabled: { type: Boolean, default: true }
-}, { versionKey: false });
+  name: String,
+  date: String,   // yyyy-mm-dd hoặc null
+  time: String,   // HH:mm
+  device: String, // fan, led1..led4, curtain
+  topic: String,  // sẽ tự ánh xạ từ device
+  cmd: String,    // ON/OFF hoặc OPEN/CLOSE/STOP
+  enabled: Boolean
+});
 
 module.exports = mongoose.model("Schedule", ScheduleSchema);
