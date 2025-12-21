c9308621cc3c1bf9d71f1b9672f981e85f443e83
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-21 18:56:48 +0700

    Update Schedule.js

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
+const scheduleSchema = new mongoose.Schema({
+  deviceId: String,
+  date: String,
+  time: String,
+  action: String
+});
+module.exports = mongoose.model("Schedule", scheduleSchema);
 
