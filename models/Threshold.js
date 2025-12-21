bfc5d6f6900e95f44a3ace1f4b763c24b83fce32
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-21 18:56:26 +0700

    Add Threshold model with schema definition

--- models/Threshold.js
+++ models/Threshold.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+const thresholdSchema = new mongoose.Schema({
+  sensorType: String,
+  comparator: String,
+  value: Number,
+  actions: [String],
+  timeStart: String,
+  timeEnd: String
+});
+module.exports = mongoose.model("Threshold", thresholdSchema);
 
