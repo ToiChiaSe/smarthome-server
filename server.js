1739085273832b7703521b29c5edd0f9f003070c
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-21 19:42:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(session({
 }));
 
 // MongoDB connect
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/smarthome";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/smarthome";
 mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
