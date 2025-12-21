7ece2cc43e2899dcb2ece6ac8c028c04005c3568
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-22 00:05:40 +0700

    Update Threshold.js

--- models/Threshold.js
+++ models/Threshold.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
 const ThresholdSchema = new mongoose.Schema({
-  enabled: { type: Boolean, default: false },
-  temperature: {
-    min: Number, max: Number,
-    actionTopic: String,
-    actionOn: String,
-    actionOff: String
+  enabled: Boolean,
+  device: String,   // fan, led1..led4, curtain
+  date: String,     // yyyy-mm-dd hoặc null
+  time: String,     // HH:mm hoặc null
+  thresholds: {
+    temperature: { min: Number, max: Number },
+    humidity:    { min: Number, max: Number },
+    light:       { min: Number, max: Number }
   },
-  humidity: { min: Number, max: Number },
-  light: { min: Number, max: Number }
-}, { versionKey: false });
+  actionMax: String, // ON/OFF hoặc OPEN/CLOSE/STOP
+  actionMin: String
+});
 
 module.exports = mongoose.model("Threshold", ThresholdSchema);
