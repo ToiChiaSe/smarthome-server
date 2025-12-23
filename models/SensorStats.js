660711d0df24963f2491ed4b31ac085171d3b809
ToiChiaSe <hoangtruongdzz@gmail.com>
2025-12-23 16:03:12 +0700

    Update SensorStats.js

--- models/SensorStats.js
+++ models/SensorStats.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+
+const SensorStatsSchema = new mongoose.Schema({
+  date: { type: String, required: true }, // YYYY-MM-DD
+  tempMin: Number,
+  tempMax: Number,
+  tempAvg: Number,
+  humMin: Number,
+  humMax: Number,
+  humAvg: Number,
+  lightMin: Number,
+  lightMax: Number,
+  lightAvg: Number
+});
+
+module.exports = mongoose.model("SensorStats", SensorStatsSchema);
 
